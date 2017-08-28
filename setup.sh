187b9f8eb45393ca519fb3c92b7e68021f3ec365
Phillip Burch <phillip.burch@live.com>
2017-08-28 14:28:18 -0500

    Create setup file

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -s Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+
+ln -s .zshrc ~/.zshrc
+
+ln -s .vimrc ~/.vimrc
